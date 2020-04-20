ebc31cb6c27d129cb806a3d4ad7c685fcb12aba3
Mark Cascardi <mcascardi@gmail.com>
2020-04-20 05:31:11 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,6 @@
 def prime?(number)
-  if number <= 1
-    false
-  else
-    new_number = (2..(number-1)).to_a
-    new_number.none? { |n| number % n == 0 }
-  end
+  return false if number <= 1
+    
+  new_number = (2..(number-1)).to_a
+  new_number.none? { |n| number % n == 0 }
 end
