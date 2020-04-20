a0355824c491bd1fa80dbd78a1b6fda647c7280b
Mark Cascardi <mcascardi@gmail.com>
2020-04-20 05:28:21 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -2,11 +2,9 @@ require 'pry'
 
 def is_prime?(number)
   if number <= 1
-    binding.pry
     false
   else
     new_number = (2..(number-1)).to_a
-    binding.pry
     new_number.none? { |n| number % n == 0 }
   end
 end
