2825cd20f1856156fde3b0c1cb211bef50a473fd
Mark Cascardi <mcascardi@gmail.com>
2020-04-20 05:21:57 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,6 +3,6 @@ def is_prime?(number)
     false
   else
     new_number = (2..(number-1)).to_a
-    new_number.none? { |n| number % n == 0 ? true : false }
+    new_number.none? { |n| number % n == 0 }
   end
 end
