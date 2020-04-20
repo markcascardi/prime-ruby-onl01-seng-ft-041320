f74c3cab87e6bf24bbf55637758f6092f2ef4488
Mark Cascardi <mcascardi@gmail.com>
2020-04-20 05:11:36 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,8 @@
-# Add  code here!
+def is_prime?(number)
+  if number <= 1
+    false
+  else
+    new_number = (2..(number-1))
+    new_number.none? { |n| number % n == 0 ? true : false }
+  end
+end
